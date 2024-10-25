a175bf55fa89cb8effe7c96a37d98eec883ebf0e
Zaid <Zaid@gmail.com>
2024-10-25 09:39:49 +0530

    javafiles

---
+++ Room.java
@@ -0,0 +1,33 @@
+public class Room{
+private int number;
+private int area;
+private String acMachine;
+public Room()
+{
+}
+public void SetNumber(int num){
+number=num;
+}
+public void SetArea (int a){
+area=a;
+}
+public void SetACMachine(String ac){
+acMachine=ac;
+}
+public void displayData()
+{
+public void displayData(){
+System. out. println(roomNumber);
+System. out. println(area);
+System. out println(acMachine);
+}
+}
+
+class MainRoom {
+public static void main(Stringll args) {
+Room rooml = (new Room);
+roomi.setNumber (101); roomi. setArea (200);
+room.setAcMachine("Samsung"); // Setting AC machine System. out-println("Room Number: " + room1. roomNumber);
+
+System. out println("Area: " + room.area + " sqoft"):
+System. out-println("AC Machine: " + room.acMachine):
